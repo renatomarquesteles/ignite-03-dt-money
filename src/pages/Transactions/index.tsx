28cb4399e3111d362584b696e920e4b02dbf7cc8
Renato <renatomarquesteles@gmail.com>
2023-03-16 20:45:23 -0300

    feat: adds Transaction Table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { Container, PriceHighlight, Table } from './styles';
 
 export const Transactions = () => {
   return (
     <div>
       <Header />
       <Summary />
+
+      <Container>
+        <Table>
+          <tbody>
+            <tr>
+              <td width="50%">Website development</td>
+              <td>
+                <PriceHighlight variant="income">$ 12,000.00</PriceHighlight>
+              </td>
+              <td>IT</td>
+              <td>04/13/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- $ 19.00</PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>04/10/2022</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 };
