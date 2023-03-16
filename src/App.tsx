8ce6d526f9b4eaeb019637a7c392b8b91d804b2a
Renato <renatomarquesteles@gmail.com>
2023-03-16 20:03:16 -0300

    styles: initial setup with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div>App</div>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 };
